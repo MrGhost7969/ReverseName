409f5e71c6871b59d85936fa1f86b24efbbfc9ef
MrGhost7969 <66699639+MrGhost7969@users.noreply.github.com>
2022-01-15 16:26:09 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const jsdom = require('jsdom');
+const dom = new jsdom.JSDOM("")
+const jquery = require('jquery')(dom.window)
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    
+    let fullName = req.body.fullname;
+    function reverseString(str) {
+        var name = "";
+        for (var i = str.length - 1; i >= 0; i--) {
+            name += str[i];
+        }
+        return name;
+    }
+    if(reverseString(fullName)){
+        res.send(reverseString(fullName));
+    } 
+})
+
+app.listen(3000, function () {
+    console.log("Test!");
+})
